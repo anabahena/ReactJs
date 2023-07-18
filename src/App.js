16386f669c9d157bb298eca73f000fb4537ab4de
anabahena <ana.gonzalezb@ab-inbev.com>
2023-07-18 13:15:50 -0600

    renew

--- src/App.js
+++ src/App.js
@@ -2,16 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const clickMe = () => {
-    console.log('clic')
-    window.parent.postMessage(
-      JSON.stringify({ functionToRun: "soundAlert", alertMessage: 'alertMessage' }),
-      "http://localhost:3000/"
-    );
-  };
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={clickMe}>Hola Mundo</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
